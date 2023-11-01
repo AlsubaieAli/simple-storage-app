7512fffb71dbf08462c6372c82d487e159a2afc4
Ali Alsubaie <alsubaie.ali.b@gmail.com>
2023-11-01 03:41:16 +0300

    Updated docker compose and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  root to: "hello_world#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root "hello_world#index"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
